f7f242e95857fe574b2698a630321c5d5bc4da93
HudaMalik <32980018+HudaMalik@users.noreply.github.com>
2017-10-21 17:17:15 +0500

    Add game project

---
+++ anime menu/Assets/menubuttons.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+function Start ()
+ {
+	
+}
+
+function Update () 
+{
+	
+}
+
+function StartGame ()
+{
+  Application.LoadLevel("Level 01");
+	
+}
+
+function ExitGame ()
+ {
+	Application.Quit();
+}
+
+function Level ()
+ {
+	Application.LoadLevel("Level 02");
+}
+
+
+
